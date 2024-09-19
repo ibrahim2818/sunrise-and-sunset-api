f9c6c99c539941ff45cf6c4a8bc94184db767f29
ibrahim2818 <ibrahim0001@std.bdu.ac.bd>
2024-09-19 14:03:25 +0600

    modified

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import requests
 
+# Get the ISS current location
 response1 = requests.get(url="http://api.open-notify.org/iss-now.json")
-response1.raise_for_status
-lati= response1.json()["iss_position"]["latitude"]
-longi= response1.json()["iss_position"]["longitude"]
+response1.raise_for_status()  # Called as a method
+lati = response1.json()["iss_position"]["latitude"]
+longi = response1.json()["iss_position"]["longitude"]
 
-response2= requests.get(url= f"https://api.sunrise-sunset.org/json?lat={lati}&lng={longi}")
-response2.raise_for_status
-sunrise= response2.json()["results"]["sunrise"]
-sunset= response2.json()["results"]["sunset"]
-print(f"sunrise:{sunrise}\nsunset: {sunset}")
+# Parameters for the second API request
+parameters = {
+    "lat": lati,
+    "lng": longi
+}
+
+# Fetch sunrise and sunset times using the latitude and longitude
+response2 = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response2.raise_for_status()  # Called as a method
+sunrise = response2.json()["results"]["sunrise"]
+sunset = response2.json()["results"]["sunset"]
+
+# Print the results
+print(f"Sunrise: {sunrise}\nSunset: {sunset}")
